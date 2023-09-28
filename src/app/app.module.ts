405055c358535ee21b35822d7ea7824082629332
gabo10-FS <ausaldivar568@gmail.com>
2023-09-27 21:07:33 -0600

    module
    correcion del module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,51 +4,44 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-=======
 //Componentes
 import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
-//Angular Material
-import {MatButtonModule} from '@angular/material/button';
+//import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Angular material
 import {MatInputModule} from '@angular/material/input';
-import { NuevoLoginScreenComponent } from './screens/nuevo-login-screen/nuevo-login-screen.component';
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
-
+import { MatNativeDateModule } from '@angular/material/core';
+//Cambia el idioma a español
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+//Mask
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+//Options mask
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 @NgModule({
   declarations: [
     AppComponent,
     LoginScreenComponent,
-    HomeScreenComponent,
     RegistroScreenComponent,
-    NuevoLoginScreenComponent,
+    //HomeScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
     MatInputModule,
+    MatButtonModule,
     MatIconModule,
     FormsModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    NgxMaskModule.forRoot(options),
   ],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
->>>>>>> Stashed changes
   ],
   bootstrap: [AppComponent]
 })
